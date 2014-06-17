b26c2fbf5145a11a0b8c5c5911e3f4a775bb1656
Wonderfall <wonderfall@riseup.net>
2014-06-17 22:08:30 +0200

    Add a nickname generator
    Okay it's not a game, but it's still fun!

---
+++ nickgen.rb
@@ -0,0 +1,15 @@
+puts ">>> Nickname generator <<<"
+
+vowels = "aaaeeeeeiiiooouuy"
+conson = "bbbcccdddffgghhjjklllmmnnppqrrrssstttvvwxz"
+nick = String.new
+
+puts "ADVICE : choose between 3 and 12 letters will bring better results"
+print "How many ? "
+
+(gets.chomp.to_i / 2).times do
+	nick += vowels[rand(0..vowels.length)]
+	nick += conson[rand(0..conson.length)]
+end
+
+at_exit { puts "\nYour new nickname is #{nick.capitalize} !" }
