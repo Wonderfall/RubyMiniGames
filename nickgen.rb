0a98b2bbe47e5f45afc89996fe2872fe6d33c4e6
Wonderfall <wonderfall@riseup.net>
2014-06-18 18:55:06 +0200

    Avoid error "nil into String convert"

--- nickgen.rb
+++ nickgen.rb
@@ -7,8 +7,8 @@ puts "ADVICE : choose between 3 and 12 letters will bring better results"
 print "How many ? "
 
 (gets.chomp.to_i / 2).times do
-  nick += vowels[rand(0..vowels.length)]
-  nick += conson[rand(0..conson.length)]
+  nick += vowels[rand(0..vowels.length)].to_s
+  nick += conson[rand(0..conson.length)].to_s
 end
 
 at_exit { puts "\nYour new nickname is #{nick.capitalize} !" }
