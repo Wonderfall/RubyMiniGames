8e37e7e9e29d5c5571545285af0342b0b6bc5702
Wonderfall <wonderfall@riseup.net>
2014-06-18 22:37:54 +0200

    Update roshambo.rb

--- roshambo.rb
+++ roshambo.rb
@@ -1,46 +1,39 @@
 puts "\n >>> A Rock-Paper-Scissors game <<< \n"
+
+def element (number)
+  case number
+  when 1 then number = :rock
+  when 2 then number = :paper
+  when 3 then number = :scissors
+  else puts " >>> INVALID CHOICE"
+  end
+  return number
+end
+
 score, playerChoice = 0, 0
+rules = { :rock => :paper, :paper => :scissors, :scissors => :rock }
 print " >>> How many rounds : "
 
 gets.to_i.times do
 
   loop do
     print "\n >>> 1.Rock 2.Paper 3.Scissors : "
-    playerChoice = gets.chomp.to_i
-    print " > You played with the "
-    case playerChoice
-    when 1
-      puts "rock."
-      break
-    when 2
-      puts "paper."
-      break
-    when 3
-      puts "scissors."
-      break
-    else
-      puts "INVALID CHOICE"
-    end
+    playerChoice = element(gets.to_i)
+    break if playerChoice.class == Symbol
   end
 
-  print " > The AI played with the "
-  case aiChoice = rand(1..3)
-  when 1
-    puts "rock."
-  when 2
-    puts "paper."
-  when 3
-    puts "scissors."
-  end
+  aiChoice = element(rand(1..3))
+  puts " > You played with the #{playerChoice}."
+  puts " > The AI played with the #{aiChoice}."
   
-  if playerChoice == 1 && aiChoice == 3 || playerChoice == 2 && aiChoice == 1 || playerChoice == 3 && aiChoice == 2
+  case ([playerChoice, aiChoice] & rules.values_at(playerChoice, aiChoice)).first
+  when playerChoice
     puts " >>> You won !"
     score += 1
-  elsif playerChoice == aiChoice
-    puts " >>> There's no winner."
-  else
+  when aiChoice
     puts " >>> You lost !"
     score -= 1
+  else puts " >>> There's no winner."
   end
 
 end
