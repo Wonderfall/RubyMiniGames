c9e3b16fba71a541b7bba9fec2b49ecb7825d2c9
Wonderfall <wonderfall@riseup.net>
2014-06-20 13:56:41 +0200

    Update nickgen

--- nickgen.rb
+++ nickgen.rb
@@ -1,14 +1,13 @@
 puts "\n >>> Nickname generator <<<"
 
-vowels, conson = "aaaeeeeeiiiooouuy", "bbbcccdddffgghhjjklllmmnnppqrrrssstttvvwxz"
-nick = String.new
-
+vowels, conson, nick = "aaaeeeeeiiiooouuy", "bbbcccdddffgghhjjklllmmnnppqrrrssstttvvwxz", String.new
 puts "ADVICE : choose between 3 and 12 letters will bring better results"
 print "How many ? "
+i = gets.to_i
 
-(gets.to_i / 2).times do
-  nick += vowels[rand(0..vowels.length)].to_s
-  nick += conson[rand(0..conson.length)].to_s
+until nick.length == i
+	nick += vowels[rand(vowels.length-1)]
+	nick += conson[rand(conson.length-1)]
 end
 
 at_exit { puts "\nYour new nickname is #{nick.capitalize} !" }
